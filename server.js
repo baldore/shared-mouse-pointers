800656ef6997a76b8bffd41766a699996e600a41
baldore <acornssn@gmail.com>
2016-12-24 23:12:00 -0500

    Add standard linter

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-'use strict';
+'use strict'
 
-const app = require('express')();
-const port = 3000;
+const app = require('express')()
+const port = 3000
 
 app.get('/', function (req, res) {
-  res.sendFile(`${__dirname}/views/index.html`);
-});
+  res.sendFile(`${__dirname}/views/index.html`)
+})
 
 app.listen(port, function () {
-  console.log(`Server started in port ${port}`);
-});
+  console.log(`Server started in port ${port}`)
+})
