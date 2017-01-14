226586b9964f3091562a00633dfd8283eb0c6eb9
Camilo Orrego <aorrego@hugeinc.com>
2017-01-14 00:50:24 -0500

    Add first view - form for pointer name

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,15 @@
   'use strict'
 
   const socket = io.connect('http://localhost:3000')
+
   socket.on('news', function (data) {
     console.log(data)
+  })
+
+  const form = document.querySelector('#first-view form')
+
+  form.addEventListener('submit', function onFormSubmit (e) {
+    e.preventDefault()
+    console.log('submitting')
   })
 })()
