79797049b1906cf98b5cc69ab01230d977a91dde
Camilo Orrego <aorrego@hugeinc.com>
2017-01-15 01:41:24 -0500

    Add react for components

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,137 @@
-/* global Rx */
+/* global Rx, io, React, ReactDOM */
+/* eslint-disable space-before-function-paren */
 (() => {
   'use strict'
 
-  const firstView = document.querySelector('#first-view')
-  const secondView = document.querySelector('#second-view')
-  const form = firstView.querySelector('form')
-  const input = form.querySelector('input[type="text"]')
+  const r = React.createElement.bind(React)
 
-  const inputValueOnSubmit$ = Rx.Observable.fromEvent(form, 'submit')
-    .do((e) => e.preventDefault())
-    .map(() => input.value.trim())
-    .filter((string) => string !== '')
+  class App extends React.Component {
+    constructor(props) {
+      super(props)
 
-  inputValueOnSubmit$.subscribe(
-    () => {
-      firstView.remove()
-      secondView.classList.remove('hidden')
+      this.onFormSubmit = this.onFormSubmit.bind(this)
+
+      this.state = {
+        username: null
+      }
+
+      this.socket = null
     }
-  )
+
+    onFormSubmit(username) {
+      this.setState({ username })
+    }
+
+    render() {
+      const { username } = this.state
+
+      return (
+        r('div', null, [
+          !username
+            ? r(SetUsernameView, { onSubmit: this.onFormSubmit })
+            : r(SharedPointersView, { username })
+        ])
+      )
+    }
+  }
+
+  class SetUsernameView extends React.Component {
+    constructor(props) {
+      super(props)
+
+      this.setUsername = this.setUsername.bind(this)
+    }
+
+    setUsername(e) {
+      e.preventDefault()
+
+      const value = this.inputRef.value.trim()
+
+      if (value) {
+        this.props.onSubmit(value)
+      }
+    }
+
+    render() {
+      return (
+        r('div', { className: 'full-screen-view' }, [
+          r('form', { onSubmit: this.setUsername }, [
+            r('label', { className: 'form-group' }, [
+              r('span', { className: 'form-label' }, `What's your name?`),
+              r('input', {
+                className: 'form-input',
+                type: 'text',
+                name: 'username',
+                ref: (input) => (this.inputRef = input)
+              })
+            ])
+          ])
+        ])
+      )
+    }
+  }
+
+  class SharedPointersView extends React.Component {
+    constructor(props) {
+      super(props)
+
+      this.socket = null
+      this.pointerMoveSubscription = null
+    }
+
+    componentDidMount() {
+      this.socket = io.connect('http://localhost:3000')
+      const pointerMove$ = Rx.Observable.fromEvent(document, 'mousemove')
+        .map((e) => ({
+          username: this.props.username,
+          x: Math.floor((e.clientX / window.innerWidth).toFixed(2) * 100),
+          y: Math.floor((e.clientY / window.innerHeight).toFixed(2) * 100)
+        }))
+        .throttle(40)
+
+      this.pointerMoveSubscription = pointerMove$.subscribe(
+        (data) => this.socket.emit('pointer-move', data)
+      )
+    }
+
+    componentWillUnmount() {
+      this.socket.disconnect()
+      this.pointerMoveSubscription()
+    }
+
+    render() {
+      return (
+        r('div', null, [
+          r('div', { className: 'fixed-panel' }, [
+            r('p', null, `x (percentage):`),
+            r('p', null, `y (percentage):`)
+          ])
+        ])
+      )
+    }
+  }
+
+  ReactDOM.render(React.createElement(App), document.querySelector('#app'))
+
+  // socket.on('response', (data) => console.log('taaake your data', data))
+  //
+  // const pointerMove$ = inputValueOnSubmit$
+  //   .flatMap((username) =>
+  //     Rx.Observable.fromEvent(document, 'mousemove')
+  //       .map((e) => ({
+  //         username,
+  //         x: Math.floor((e.clientX / window.innerWidth).toFixed(2) * 100),
+  //         y: Math.floor((e.clientY / window.innerHeight).toFixed(2) * 100)
+  //       }))
+  //   )
+  //   .throttle(40)
+  //
+  // pointerMove$.subscribe(
+  //   (data) => {
+  //     socket.emit('pointer-move', data)
+  //     // Maybe this should be done in the response, since the data is being already sent
+  //     xCoord.innerText = `${data.x}%`
+  //     yCoord.innerText = `${data.y}%`
+  //   }
+  // )
 })()
--- server.js
+++ server.js
@@ -4,16 +4,34 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
+const Rx = require('rx')
 
 const port = 3000
 
 app.use(express.static('public'))
 
-io.on('connection', function onIOConnection (socket) {
-  socket.emit('news', { hello: 'world' })
-  socket.on('my-other-event', (data) => console.log(data))
-})
-
 server.listen(port, function () {
   console.log(`Server started in port ${port}`)
 })
+
+const client$ = Rx.Observable.create((observer) => {
+  io.on('connection', (client) => observer.onNext(client))
+})
+
+const clientMovePointer$ = client$
+  .flatMap((client) => Rx.Observable.create(
+    (observer) => client.on('pointer-move', observer.onNext.bind(observer)))
+  )
+
+clientMovePointer$.subscribe(
+  (data) => console.log(data.username, data)
+)
+
+// client$.subscribe(
+//   (client) => {
+//     client.on('pointer-move', (data) => {
+//       console.log(data)
+//       io.sockets.emit('response', 'it works....')
+//     })
+//   }
+// )
