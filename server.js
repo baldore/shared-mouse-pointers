44ece117eb77e130c7803bead65287279eaebbc7
Camilo Orrego <aorrego@hugeinc.com>
2017-01-20 17:37:25 -0500

    Remove unnecessary line

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
-
 const Rx = require('rx')
 
 const port = 3000
